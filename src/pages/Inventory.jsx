3623a4e33234153a45dc0f49897bd2a5b58d7241
Rahul Rameshwar Rathod <rahulrathod741258@gmail.com>
2025-01-16 14:05:28 +0530

    logout problem is solve

--- src/pages/Inventory.jsx
+++ src/pages/Inventory.jsx
@@ -139,9 +139,11 @@ const Inventory = () => {
   // Logout function
   const logout = () => {
     localStorage.removeItem("token");
-    navigate("/login");
-  };
+    navigate("/");
 
+   
+  };
+  
   return (
     <div className="min-h-screen bg-gradient-to-r from-gray-800 via-gray-900 to-black text-white p-6">
       <h2 className="text-4xl font-extrabold text-center mb-12 text-white tracking-wide">
